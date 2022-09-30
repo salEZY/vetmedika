2a149d0abd2ed52b40d7d5612af6a11b074b28cc
Aleksandar Bobic <salezybob@gmail.com>
2022-09-30 13:52:28 +0200

    add to top btn, footer component + refactor

--- src/components/AllPages/AllPages.js
+++ src/components/AllPages/AllPages.js
@@ -3,6 +3,8 @@ import Action from "../Action/Action";
 import Contact from "../Contact/Contact";
 import Home from "../Home/Home";
 import Services from "../Services/Services";
+import Footer from "../Footer/Footer";
+import ToTop from "../ToTop/ToTop";
 
 const AllPages = () => {
   return (
@@ -11,6 +13,8 @@ const AllPages = () => {
       <Action />
       <Services />
       <Contact />
+      <Footer />
+      <ToTop />
     </>
   );
 };
